841e38ee5338f50f75f33f22330f25c912fc142b
Weinan Liu <137690421+Lewis2024@users.noreply.github.com>
2024-04-24 18:29:49 +0800

    Create recommendation.vue

---
+++ springboot/vue/recommendation.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <button @click="getRecommendations">获取推荐</button>
+    <div v-for="(recommendation, index) in recommendations" :key="index">
+      {{ recommendation }}
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      recommendations: []
+    };
+  },
+  methods: {
+    getRecommendations() {
+      axios.get('http://localhost:8080/recommendations?userId=i')
+        .then(response => {
+          this.recommendations = response.data;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  }
+};
+</script>
